2366717989e604f42822194256fd6ada907fdc4f
gabriel_payano <gabriel21kun@gmail.com>
2019-04-28 17:40:16 +0200

    "Agregamos un try catch al método analizarGeneracion()"

--- src/juego/Juego_Vida.java
+++ src/juego/Juego_Vida.java
@@ -22,16 +22,14 @@ public class Juego_Vida {
     private Integer contador = 0;
     private final Integer contadorGenAnterior = contador;
 
-     /*Método principal que se encarga de realizar las llamadas a los diferentes métodos del juego*/
-    public void EjecutarJuego(){
+    /*Método principal que se encarga de realizar las llamadas a los diferentes métodos del juego*/
+    public void EjecutarJuego() {
         menuInicio();
         imprimirGeneracion1();
         generacionSiguiente();
-        
-        
+
     }
-    
-    
+
     //método que se encarga de dar la bienvenida al juego al usuario y que además sirve para seleccionar las primeras opciones de creación de la matriz de celulas
     public void menuInicio() {
         Scanner teclado = new Scanner(System.in);
@@ -51,8 +49,8 @@ public class Juego_Vida {
         }
 
     }
-    
-      /*ANALIZAR_GENERACION*/
+
+    /*ANALIZAR_GENERACION*/
  /*Método que aplica la lógica del juego revisando la matriz de celulas y cambiando el estado de la celula analizada en base a la cantidad de celulas vivas o muertas
     que se encuentren a su alrededor*/
  /*POSICIONES*/
@@ -69,99 +67,103 @@ public class Juego_Vida {
         int contadorVivas = 0; // cuenta el número de celulas vivas alrededor de una celula en concreto
         int contadorMuertas = 0; // cuenta el número de celulas muertas alrededor de una celula en concreto
         Celula[][] aux = uno.getMatrizCelula();
-        for (int i = 0; i < uno.getMatrizCelula().length; i++) {
-            for (int j = 0; j < uno.getMatrizCelula().length; j++) {
 
-                /*Recorremos la matriz y viendo el estado de las posiciones que se encuentran alrededor de i,j que es la celula que estamos analizando
+        try {
+
+            for (int i = 0; i < uno.getMatrizCelula().length; i++) {
+                for (int j = 0; j < uno.getMatrizCelula().length; j++) {
+
+                    /*Recorremos la matriz y viendo el estado de las posiciones que se encuentran alrededor de i,j que es la celula que estamos analizando
                 En caso de que esten vivas se suma un valor al contador de celulas vivas, por otro lado si estan muertas se suma el valor al contadorMuestas*/
  /*CONTADORES*/
-                //izquierda esta viva
-                if (aux[i][j - 1].isEstado() == true) {
-                    contadorVivas++;
-                    //Izquierda muerta
-                } else if (aux[i][j - 1].isEstado() == false) {
-                    contadorMuertas++;
-                }
-                // derecha esta viva
-                if (aux[i][j + 2].isEstado() == true) {
-                    contadorVivas++;
-                    //derecha mmuerta
-                } else if (aux[i][j + 2].isEstado() == false) {
-                    contadorMuertas++;
-                }
-                // si arriba esta viva
-                if (aux[i - 1][j].isEstado() == true) {
-                    contadorVivas++;
-                    //arriba muerta
-                } else if (aux[i - 1][j].isEstado() == false) {
-                    contadorMuertas++;
-                }
-                // si abajo esta viva
-                if (aux[i + 1][j].isEstado() == true) {
-                    contadorVivas++;
-                    //abajo esta muerta
-                } else if (aux[i + 1][j].isEstado() == false) {
-                    contadorMuertas++;
-                }
+                    //izquierda esta viva
+                    if (aux[i][j - 1].isEstado() == true) {
+                        contadorVivas++;
+                        //Izquierda muerta
+                    } else if (aux[i][j - 1].isEstado() == false) {
+                        contadorMuertas++;
+                    }
+                    // derecha esta viva
+                    if (aux[i][j + 2].isEstado() == true) {
+                        contadorVivas++;
+                        //derecha mmuerta
+                    } else if (aux[i][j + 2].isEstado() == false) {
+                        contadorMuertas++;
+                    }
+                    // si arriba esta viva
+                    if (aux[i - 1][j].isEstado() == true) {
+                        contadorVivas++;
+                        //arriba muerta
+                    } else if (aux[i - 1][j].isEstado() == false) {
+                        contadorMuertas++;
+                    }
+                    // si abajo esta viva
+                    if (aux[i + 1][j].isEstado() == true) {
+                        contadorVivas++;
+                        //abajo esta muerta
+                    } else if (aux[i + 1][j].isEstado() == false) {
+                        contadorMuertas++;
+                    }
 
-                // si la esquina superior izquierda esta viva
-                if (aux[i - 1][j - 1].isEstado() == true) {
-                    contadorVivas++;
-                    // muerta
-                } else if (aux[i - 1][j - 1].isEstado() == false) {
-                    contadorMuertas++;
-                }
-                //Si la esquina superior derecha esta viva
-                if (aux[i - 1][j + 1].isEstado() == true) {
-                    contadorVivas++;
-                }
-                // si la esquina inferior izquierda esta viva
-                if (aux[i + 1][j - 1].isEstado() == true) {
-                    contadorVivas++;
-                    //muerta
-                } else if (aux[i + 1][j - 1].isEstado() == false) {
-                    contadorMuertas++;
-                }
-                // si la esquina inferior derecha esta viva
-                if (aux[i + 1][j + 1].isEstado() == true) {
-                    contadorVivas++;
-                    //muerta
-                } else if (aux[i + 1][j + 1].isEstado() == false) {
-                    contadorMuertas++;
-                }
+                    // si la esquina superior izquierda esta viva
+                    if (aux[i - 1][j - 1].isEstado() == true) {
+                        contadorVivas++;
+                        // muerta
+                    } else if (aux[i - 1][j - 1].isEstado() == false) {
+                        contadorMuertas++;
+                    }
+                    //Si la esquina superior derecha esta viva
+                    if (aux[i - 1][j + 1].isEstado() == true) {
+                        contadorVivas++;
+                    }
+                    // si la esquina inferior izquierda esta viva
+                    if (aux[i + 1][j - 1].isEstado() == true) {
+                        contadorVivas++;
+                        //muerta
+                    } else if (aux[i + 1][j - 1].isEstado() == false) {
+                        contadorMuertas++;
+                    }
+                    // si la esquina inferior derecha esta viva
+                    if (aux[i + 1][j + 1].isEstado() == true) {
+                        contadorVivas++;
+                        //muerta
+                    } else if (aux[i + 1][j + 1].isEstado() == false) {
+                        contadorMuertas++;
+                    }
 
-                System.out.println("Contador vivas: " + contadorVivas);
-                System.out.println("Contador muertas: " + contadorMuertas);
-                /*CONDICIONES*/
+                    System.out.println("Contador vivas: " + contadorVivas);
+                    System.out.println("Contador muertas: " + contadorMuertas);
+                    /*CONDICIONES*/
  /*si el contador de celulas vivas es mayor o igual que 3 y la celula esta viva, entonces la celula muere por sobrepoblación pero si esta esta muerta resucita*/
-                if (contadorVivas >= 3 && aux[i][j].isEstado() == true) {
-                    aux[i][j].matarCelula();
-                } else if (contadorVivas >= 3 && aux[i][j].isEstado() == false) {
-                    aux[i][j].resucitarCelula();
-                }
-                /*si el contador de celulas vivas es igual que 3 y la celula esta muerta entonces la celula vuelve a vivir (estado =true) */
-                if (contadorVivas == 3 && aux[i][j].isEstado() == false) {
-                    aux[i][j].resucitarCelula();
-                }
+                    if (contadorVivas >= 3 && aux[i][j].isEstado() == true) {
+                        aux[i][j].matarCelula();
+                    } else if (contadorVivas >= 3 && aux[i][j].isEstado() == false) {
+                        aux[i][j].resucitarCelula();
+                    }
+                    /*si el contador de celulas vivas es igual que 3 y la celula esta muerta entonces la celula vuelve a vivir (estado =true) */
+                    if (contadorVivas == 3 && aux[i][j].isEstado() == false) {
+                        aux[i][j].resucitarCelula();
+                    }
 
-                /*si la celula esta viva y tiene 1 o 0 celulas vivas a su alrededor muere por soledad*/
-                if (contadorVivas <= 1 && aux[i][j].isEstado() == true) {
-                    aux[i][j].matarCelula();
-                }
+                    /*si la celula esta viva y tiene 1 o 0 celulas vivas a su alrededor muere por soledad*/
+                    if (contadorVivas <= 1 && aux[i][j].isEstado() == true) {
+                        aux[i][j].matarCelula();
+                    }
 
-                /*Una célula viva con 2 ó 3 células vecinas vivas sigue viva.*/
-                if (contadorVivas == 2 || contadorVivas == 3 && aux[i][j].isEstado() == true) {
-                    aux[i][j].resucitarCelula();
-                }
+                    /*Una célula viva con 2 ó 3 células vecinas vivas sigue viva.*/
+                    if (contadorVivas == 2 || contadorVivas == 3 && aux[i][j].isEstado() == true) {
+                        aux[i][j].resucitarCelula();
+                    }
 
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("");
         }
 
         uno.setMatrizCelula(aux); // aplicamos los cambios realizados sobre la matriz auxiliar y en la generación 1
     }
 
-    
-    
     // método que genera la matriz de celula en base al número entero que introduce el usuario por teclado
     // el máximo valor que puede tomar este número es 25 y el minimo 7
     public void generacionPorValor() {
@@ -190,7 +192,7 @@ public class Juego_Vida {
     }
 
     public void generacionAleatoria() {
-       
+
 //        opcion = 2;
         int numero = Generacion.tamañoAleatorioMatriz();
         if (numero <= 70) {
@@ -225,11 +227,11 @@ public class Juego_Vida {
         }
 
     }
-    
+
     //Método que imprime la segunda matriz de celulas
-    public void imprimirGeneracion2(){
-        
-           Celula[][] aux = dos.copiarMatriz();
+    public void imprimirGeneracion2() {
+
+        Celula[][] aux = dos.copiarMatriz();
 
         for (int x = 0; x < dos.getMatrizCelula().length; x++) {
             for (int y = 0; y < aux[x].length; y++) {
@@ -242,38 +244,35 @@ public class Juego_Vida {
             System.out.println();
 
         }
-        
+
     }
 
-   
-    
     // método que se encarga de gestionar los cambios que sufren las generaciones  al seleccionar la opcion de "siguiente generación"
-    public void gestionGeneracion(){
-        dos=uno;// pasamos el estado de la generacion 1 a la generación 2 para luego mostrarla por pantallay asi poder ver los cambios
+    public void gestionGeneracion() {
+        dos = uno;// pasamos el estado de la generacion 1 a la generación 2 para luego mostrarla por pantallay asi poder ver los cambios
         analizarGeneracion(); // aplicamos la logica del programa sobre la generación 1 y así ver los cambios en la matriz
-        
+
     }
-    
-    public void generacionSiguiente(){
+
+    public void generacionSiguiente() {
         this.contador++;
         Scanner teclado = new Scanner(System.in);
         System.out.println("¿Quieres generar la siguiente generación?");
         System.out.println(" \n S) Si");
         System.out.println(" N) No");
-        
-        String opcion= teclado.nextLine(); // guarda la opción seleccioanda por el usuario
-        
+
+        String opcion = teclado.nextLine(); // guarda la opción seleccioanda por el usuario
+
         if (opcion.equalsIgnoreCase("S")) {
             gestionGeneracion();
             System.out.println("\n Generación actual: " + this.contador);
             imprimirGeneracion1();
             System.out.println(" \n Generación anterior" + this.contadorGenAnterior);
             imprimirGeneracion2();
-        }else {
+        } else {
             JOptionPane.showMessageDialog(null, "El programa ha finalizado");
         }
-        
-        
+
     }
 
 
@@ -317,7 +316,6 @@ public class Juego_Vida {
 //    public void setOpcion(int opcion) {
 //        this.opcion = opcion;
 //    }
-
     public Integer getContador() {
         return contador;
     }
